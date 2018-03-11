02737e75b9951cba7a6d4fed3c40bc06c43bed66
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-03-11 11:50:07 +0200

    add rotate function

--- script.js
+++ script.js
@@ -100,16 +100,41 @@ function playerDrop() {
     dropCounter = 0;
 }
 
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ]
+        }   
+    }
+    if(dir > 0) {
+        matrix.forEach(row => {row.reverse()})
+    } else {
+        matrix.reverse();
+    }
+}
+
+function playerRotate(dir) {
+    rotate(player.matrix, dir);
+}
+
 document.addEventListener('keydown', (e) => {
     if(e.keyCode === 37){
        playerMove(-1);
-    };
-    if(e.keyCode === 39){
+    } else if(e.keyCode === 39) {
         playerMove(1);
-    };
-    if(e.keyCode === 40){
+    } else if(e.keyCode === 40) {
        playerDrop();
-    };    
+    } else if(e.keyCode === 81) {
+        playerRotate(-1);
+    } else if(e.keyCode === 87) {
+        playerRotate(1);
+    }    
 });
 
 update();
