57d6df6072537e10ae8ebb878fe251771e23112e
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-03-11 12:32:20 +0200

    update playerMove function

--- script.js
+++ script.js
@@ -120,7 +120,18 @@ function rotate(matrix, dir) {
 }
 
 function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
     rotate(player.matrix, dir);
+    while(collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if(offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
 }
 
 document.addEventListener('keydown', (e) => {
